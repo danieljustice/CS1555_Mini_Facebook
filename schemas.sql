7ed6b5ffffbfe30033c4e2065726cb38457e3957
Jordan Carr <jcc117@pitt.edu>
2017-10-30 14:21:24 -0400

    Added next 3 tables

--- schemas.sql
+++ schemas.sql
@@ -19,5 +19,32 @@ CREATE TABLE profile(
 	password varchar2(50),
 	date_of_birth date,
 	lastlogin timestamp,
-	CONSTRAINT profile_pk PRIMARY KEY (userID)
-);
+	CONSTRAINT profile_pk PRIMARY KEY (userID));
+
+CREATE TABLE friends(
+	userID1 varchar2(20),
+	userID2 varchar2(20),
+	JDate date,
+	message varchar2(200),
+	CONSTRAINT friends_pk PRIMARY KEY(userID1, userID2, JDate),
+	CONSTRAINT friends_fk1 FOREIGN KEY (userID1) REFERENCES profile(userID),
+	CONSTRAINT friends_fk FOREIGN KEY (userID2) REFERENCES profile(userID));
+
+CREATE TABLE pendingFriends(
+	fromID varchar2(20),
+	toID varchar2(20),
+	message varchar2(200),
+	CONSTRAINT pendingFriends_pk PRIMARY KEY (fromID, toID),
+	CONSTRAINT pendingFriends_FK1 FOREIGN KEY (fromID) REFERENCES profile(userID),
+	CONSTRAINT pendingFriends_FK2 FOREIGN KEY (toID) REFERENCES profile(userID));
+
+CREATE TABLE messages(
+	msgID varchar2(20),
+	fromID varchar2(20),
+	message varchar2(200),
+	toUserID varchar2(20),
+	toGroupID varchar2(20),
+	dateSend date,
+	CONSTRAINT messages_pk PRIMARY KEY (msgID),
+	CONSTRAINT messages_fk1 FOREIGN KEY (toUserID) REFERENCES profile(userID),
+	CONSTRAINT messages_fk2 FOREIGN KEY (toGroupID) REFERENCES groups(gID));
