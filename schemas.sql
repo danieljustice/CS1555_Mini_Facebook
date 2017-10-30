39eac95ef23957e8bbfc3684d315c83b6c614ef0
Speedy Turtle <danieljustice09@gmail.com>
2017-10-30 14:36:21 -0400

    added skeleton of lower half of tables

--- schemas.sql
+++ schemas.sql
@@ -21,3 +21,49 @@ CREATE TABLE profile(
 	lastlogin timestamp,
 	CONSTRAINT profile_pk PRIMARY KEY (userID)
 );
+
+
+
+
+
+
+
+
+CREATE TABLE messageRecipient(
+	msgID			varchar2(20),
+	userID			varchar2(20),
+	CONSTRAINT messageRecipient_pk PRIMARY KEY (msgID, useID),
+	CONSTRAINT messageRecipient_fk1 FOREIGN KEY (msgID) REFERENCES messages(msgID),
+	CONSTRAINT messageRecipient_fk2 FOREIGN KEY (userID) REFERENCES profile(userID)
+);
+
+
+CREATE TABLE groups(
+	gID				varchar2(20),
+	name			varchar2(50),
+	description		varchar2(200),
+	CONSTRAINT groups_ID PRIMARY KEY (gID)
+);
+
+CREATE TABLE groupMembership(
+	gID				varchar2(20),
+	--user that is a part of the group
+	userID			varchar2(20),
+	--roles are manager or member
+	role			varchar2(20),
+	--primary key is a combo of the group ID and a user in that group
+	CONSTRAINT groupMembership_pk PRIMARY KEY (gID, userID),
+	CONSTRAINT groupMembership_fk1 FOREIGN KEY gID REFERENCES groups(gID),
+	CONSTRAINT groupMembership_fk2 FOREIGN KEY userID REFERENCES profile(userID)
+);
+
+CREATE TABLE pendingGroupmembers(
+	gID				varchar2(20),
+	--user who wants to joing the group
+	userID			varchar2(20),
+	message 		varchar2(200),
+	--primary key is a combo of the group to be joined and the user that wants to join
+	CONSTRAINT pendingGroupmembers_pk PRIMARY KEY (gID, userID),
+	CONSTRAINT pendingGroupmembers_fk1 FOREIGN KEY gID REFERENCES groups(gID),
+	CONSTRAINT pendingGroupmembers_fk2 FOREIGN KEY userID REFERENCES profile(userID)
+);
