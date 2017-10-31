b7f35c72e95024551e3c02ab95d2cde24ad41b70
Jordan Carr <jcc117@pitt.edu>
2017-10-31 00:39:24 -0400

    Modded test data

--- testdata.sql
+++ testdata.sql
@@ -15,3 +15,32 @@ INSERT INTO profiles values('a8', 'Jarred', '1', null, null);
 INSERT INTO profiles values('a9', 'Jarret', '1', null, null);
 INSERT INTO profiles values('a10', 'Jarrett', '1', null, null);
 --10
+INSERT INTO profiles values('a11', 'Max', '1', null, null);
+INSERT INTO profiles values('a12', 'Mike', '1', null, null);
+INSERT INTO profiles values('a13', 'Mac', '1', null, null);
+INSERT INTO profiles values('a14', 'Manny', '1', null, null);
+INSERT INTO profiles values('a15', 'Mary', '1', null, null);
+INSERT INTO profiles values('a16', 'Mel', '1', null, null);
+INSERT INTO profiles values('a17', 'Mellisa', '1', null, null);
+INSERT INTO profiles values('a18', 'Mark', '1', null, null);
+INSERT INTO profiles values('a19', 'Marc', '1', null, null);
+INSERT INTO profiles values('a20', 'Marcus', '1', null, null);
+--20
+INSERT INTO profiles values('a21', 'John', '1', null, null);
+INSERT INTO profiles values('a22', 'Joe', '1', null, null);
+INSERT INTO profiles values('a23', 'Jo', '1', null, null);
+INSERT INTO profiles values('a24', 'Johnny', '1', null, null);
+INSERT INTO profiles values('a25', 'Jon', '1', null, null);
+INSERT INTO profiles values('a26', 'Jerry', '1', null, null);
+INSERT INTO profiles values('a27', 'Jared', '1', null, null);
+INSERT INTO profiles values('a28', 'Jarred', '1', null, null);
+INSERT INTO profiles values('a29', 'Jarret', '1', null, null);
+INSERT INTO profiles values('a30', 'Jarrett', '1', null, null);
+--30
+--40
+--50
+--60
+--70
+--80
+--90
+--100
