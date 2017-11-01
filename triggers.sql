a67cd8abe339ebc917705a2570bd423c0d47c8c0
Speedy Turtle <danieljustice09@gmail.com>
2017-11-01 14:34:44 -0400

    added FRIENDSCANTBEPENDING trigger

--- triggers.sql
+++ triggers.sql
@@ -1,1 +1,14 @@
 --Here tha be Triggaa's!
+
+
+CREATE OR REPLACE TRIGGER
+AFTER INSERT ON friends
+FOR EACH ROW
+BEGIN 
+    DELETE FROM pendingFriends
+    WHERE (:new.userID1 = fromID AND
+            :new.userID2 = toID) OR
+            (:new.userID1 = toID AND
+            :new.userID2 = fromID);
+END;
+/
