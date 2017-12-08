d01185ac304daf54751c3a2b71b3b6bf51836dc4
Speedy Turtle <danieljustice09@gmail.com>
2017-12-08 16:11:58 -0500

    added jordan to scpfile

--- SCPfiles.py
+++ SCPfiles.py
@@ -3,6 +3,12 @@ import os
 server = input("What server are you using? (e.g. 'daj61@unixs.cis.pitt.edu')") or "daj61@unixs.cis.pitt.edu"
 filepath = input("What is the file path for these files?") or "/afs/pitt.edu/home/d/a/daj61/private/Team"
 
+# JORDAN - fill in the file path yourself
+if(server == '1'):
+    server = "jcc117@unixs.cis.pitt.edu"
+    filepath = ""
+
+
 # scps = []
 files = ["Database.java", "TestDriver.java", "SocialPanther.java", "initDB.sql", "profile.sql", "groups.sql", "friends.sql", "schemas.sql", "triggers.sql"]
 
