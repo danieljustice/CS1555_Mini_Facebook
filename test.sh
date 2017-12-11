73c7d57e6abee77e851dbf12c1d86467933a9d73
Jordan Carr <jcc117@pitt.edu>
2017-12-10 19:54:28 -0500

    testing is a bit cleaner

--- test.sh
+++ test.sh
@@ -5,8 +5,13 @@
 # sqlplus and launching the ttest.sql file
 
 javac SocialPanther.java
+echo "Testing profile creation, login, logoff"
 java SocialPanther<benchmarks/bench1.txt
+echo "Testing initiateFrienship, confirmFriendship, displayFriends"
 java SocialPanther<benchmarks/bench2.txt
+echo "Testing createGroup, initateAddingGroup, sendMessageToGroup"
 java SocialPanther<benchmarks/bench3.txt
+echo "Testing sendMessageToUser, displayMessages, displayNewMessages, topMessages"
 java SocialPanther<benchmarks/bench4.txt
+echo "Testing threeDegrees, searchForUser, dropUser"
 java SocialPanther<benchmarks/bench5.txt
